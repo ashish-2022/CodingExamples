c4b8291a4ed5edac2881d4e8fcb3515a78b4d6c7
ashish-harman <ashish.shirodkar@outlook.com>
2017-10-07 17:29:12 +0530

    Add Makefile in directories

--- C_and_CPP/Design_Pattern/compile.sh
+++
@@ -1,3 +0,0 @@
-#g++ -std=c++0x -Wall -Wextra -pedantic BuilderPattern.cpp -o BuilderPattern.o
-g++ -std=c++14 -Wall -Wextra -pedantic BuilderPattern.cpp -o BuilderPattern.o
-
--- C_and_CPP/Multithreading/compile.sh
+++
@@ -1,7 +0,0 @@
-g++ -pthread ConditioVariablesT.cpp -o ConditioVariablesT.o
-g++ -pthread DetachedThread.cpp -o DetachedThread.o
-g++ -pthread IntegerPthread.cpp -o IntegerPthread.o
-g++ -pthread manyThreads.cpp -o manyThreads.o
-g++ -pthread mutexThreads.cpp -o mutexThreads.o
-g++ -pthread pthreadExample.cpp -o pthreadExample.o
-g++ -pthread valueThreads.cpp -o valueThreads.o
